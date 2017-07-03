7da0bc4e4ffcbbb5b1f5346c15268047d0dce20c
petorious <petorious@gmail.com>
2017-07-03 16:21:55 -0500

    Attempt to fix grid jumping bug

--- src/components/widgets/CardFun.jsx
+++ src/components/widgets/CardFun.jsx
@@ -15,7 +15,7 @@ const CardFun = () =>
       showExpandableButton
     />
     <CardText expandable={true}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus euismod arcu at faucibus. Praesent luctus est sit amet orci sollicitudin tincidunt. Mauris consectetur diam ac nunc auctor, sit amet elementum enim lobortis. Duis eu libero feugiat, volutpat enim ut, varius erat. Phasellus venenatis felis et consequat viverra. Donec suscipit tempus nulla malesuada mattis. Cras quam justo, vestibulum at mauris eu, egestas lobortis tortor. Cras ac vulputate massa, sed ornare nisi. Aenean tincidunt risus auctor, gravida leo sed, ornare quam. Proin malesuada imperdiet elit in semper. Integer vitae tellus ultrices, tempus nisi in, porttitor augue. Vestibulum vitae eleifend elit, et.
+      Susie neeeds her sword back!  Mauris consectetur diam ac nunc auctor, sit amet elementum enim lobortis. Duis eu libero feugiat, volutpat enim ut, varius erat. Phasellus venenatis felis et consequat viverra. Donec suscipit tempus nulla malesuada mattis. Cras quam justo, vestibulum at mauris eu, egestas lobortis tortor. Cras ac vulputate massa, sed ornare nisi. Aenean tincidunt risus auctor, gravida leo sed, ornare quam. Proin malesuada imperdiet elit in semper. Integer vitae tellus ultrices, tempus nisi in, porttitor augue. Vestibulum vitae eleifend elit, et.
     </CardText>
   </Card>
 ;
